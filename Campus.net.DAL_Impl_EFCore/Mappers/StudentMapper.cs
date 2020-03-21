5f488ece1f9d65e050a1a626ea598fe41a138ec5
KolaCation <arharovoversteam@gmail.com>
2020-03-21 21:23:47 +0200

    Add IMapper interface

---
+++ Campus.net.DAL_Impl_EFCore/Mappers/StudentMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Campus.net.DAL_Impl_EFCore.DbModels.MainData;
+using Campus.net.Domain.MainData;
+
+namespace Campus.net.DAL_Impl_EFCore.Mappers
+{
+    internal sealed class StudentMapper : IMapper<Student, StudentDbModel>
+    {
+        public StudentDbModel EntityToModel(Student item)
+        {
+            return null;
+        }
+
+        public Student EntityToModel(StudentDbModel item)
+        {
+            return null;
+        }
+    }
+}
