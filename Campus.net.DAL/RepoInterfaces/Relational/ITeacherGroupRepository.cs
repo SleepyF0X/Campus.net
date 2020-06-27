761a7c9886b5d573fd60b38bc2f4a58b97f43356
SleepyF0X <kotohomka@gmail.com>
2020-06-27 14:43:40 +0300

    Add Repository Interfaces

---
+++ Campus.net.DAL/RepoInterfaces/Relational/ITeacherGroupRepository.cs
@@ -0,0 +1,11 @@
+﻿using Campus.net.Domain.RelationClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campus.net.DAL.RepoInterfaces.Relational
+{
+    public interface ITeacherGroupRepository : IRepository<TeacherGroup>
+    {
+    }
+}
