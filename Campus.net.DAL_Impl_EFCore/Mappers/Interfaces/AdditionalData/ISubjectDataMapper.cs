12360739bd99f6e531c68b826764d38a80e5366f
SleepyF0X <kotohomka@gmail.com>
2020-10-12 20:23:01 +0300

    Delete All

--- Campus.net.DAL_Impl_EFCore/Mappers/Interfaces/AdditionalData/ISubjectDataMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using Campus.net.DAL_Impl_EFCore.DbModels.AdditionalData;
-using Campus.net.Domain.AdditionalData;
-
-namespace Campus.net.DAL_Impl_EFCore.Mappers.Interfaces.AdditionalData
-{
-    internal interface ISubjectDataMapper : IMapper<SubjectData, SubjectDataDbModel>
-    {
-    }
-}
